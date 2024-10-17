c2f956c8c7866fd93ab69bd374269189c898e79b
timiajayi <timiajayi2001@gmail.com>
2024-10-17 12:45:38 +0100

    fix

--- app/Notifications/VerifyEmail.php
+++ app/Notifications/VerifyEmail.php
@@ -27,7 +27,8 @@ class VerifyEmail extends Notification
             ->subject('Verify Email Address')
             ->line('Please click the button below to verify your email address.')
             ->action('Verify Email Address', $verificationUrl)
-            ->line('If you did not create an account, no further action is required.');
+            ->line('If you did not create an account, no further action is required.')
+            ->salutation('Regards,\n' . config('app.name'));
     }
 
     protected function verificationUrl($notifiable)
