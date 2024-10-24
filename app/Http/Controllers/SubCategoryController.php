647b19e1059cd91a0724f09c3316b03d03409ddd
timiajayi <timiajayi2001@gmail.com>
2024-10-24 20:08:08 +0100

    prodcut,subcategory,product type fix

--- app/Http/Controllers/SubCategoryController.php
+++ app/Http/Controllers/SubCategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\SubCategory;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 
 class SubCategoryController extends Controller
