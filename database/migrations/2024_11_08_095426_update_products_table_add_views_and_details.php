c3a5adfafff33477d17a20a89f2bc036de1d5969
timiajayi <timiajayi2001@gmail.com>
2024-11-08 15:29:40 +0100

    features

--- database/migrations/2024_11_08_095426_update_products_table_add_views_and_details.php
+++ database/migrations/2024_11_08_095426_update_products_table_add_views_and_details.php
@@ -9,34 +9,34 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    // public function up()
-    // {
-    //     Schema::table('products', function (Blueprint $table) {
-    //         $table->string('slug')->nullable();
-    //         $table->integer('view_count')->default(0);
-    //         $table->text('composition')->nullable();
-    //         $table->json('shipping_details')->nullable();
-    //     });
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('slug')->nullable();
+            $table->integer('view_count')->default(0);
+            $table->text('composition')->nullable();
+            $table->json('shipping_details')->nullable();
+        });
     
-    //     // Generate slugs for existing products
-    //     DB::table('products')->orderBy('id')->each(function ($product) {
-    //         DB::table('products')
-    //             ->where('id', $product->id)
-    //             ->update(['slug' => Str::slug($product->name)]);
-    //     });
+        // Generate slugs for existing products
+        DB::table('products')->orderBy('id')->each(function ($product) {
+            DB::table('products')
+                ->where('id', $product->id)
+                ->update(['slug' => Str::slug($product->name)]);
+        });
     
-    //     // Now add the unique constraint
-    //     Schema::table('products', function (Blueprint $table) {
-    //         $table->unique('slug');
-    //     });
-    // }
+        // Now add the unique constraint
+        Schema::table('products', function (Blueprint $table) {
+            $table->unique('slug');
+        });
+    }
     
 
     // /**
     //  * Reverse the migrations.
     //  */
-    // public function down(): void
-    // {
-    //     //
-    // }
+    public function down(): void
+    {
+        //
+    }
 };
