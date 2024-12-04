ab211e2253490afb72bf318a50b60f52ff4eb435
timiajayi <timiajayi2001@gmail.com>
2024-12-04 12:08:54 +0000

    live

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://baraweb.waltanforte.com','http://localhost:5173'],
+    'allowed_origins' => ['https://baraweb.waltanforte.com','http://localhost:*'],
 
     'allowed_origins_patterns' => [],
 
