173625f425b7dc2dbf3ec16168f0908998135a59
timiajayi <timiajayi2001@gmail.com>
2025-01-31 17:29:34 +0100

    guest order

--- database/migrations/2024_01_30_make_user_id_nullable_in_orders_table.php
+++ database/migrations/2024_01_30_make_user_id_nullable_in_orders_table.php
@@ -8,27 +8,15 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
-            $table->string('order_number')->unique();
-            $table->decimal('subtotal', 10, 2);
-            $table->decimal('shipping_cost', 10, 2);
-            $table->decimal('total', 10, 2);
-            $table->string('status');
-            $table->string('stripe_session_id')->nullable();
-            $table->string('payment_status')->nullable();
-            $table->string('order_status')->nullable();
-            $table->string('paystack_reference')->nullable();
-            $table->json('shipping_address');
-            $table->string('email');
-            $table->string('phone');
-            $table->timestamps();
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreignId('user_id')->nullable()->change();
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreignId('user_id')->nullable(false)->change();
+        });
     }
 };
