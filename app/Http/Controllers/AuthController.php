e8a71769328bdb2602228292410608cf05ee1c32
timiajayi <timiajayi2001@gmail.com>
2025-02-12 19:24:53 +0100

    forgot

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', 'register', 'verifyEmail']]);
+        $this->middleware('auth:api', ['except' => ['login', 'register', 'verifyEmail', 'forgotPassword', 'resetPassword']]);
     }
 
     public function register(Request $request)
