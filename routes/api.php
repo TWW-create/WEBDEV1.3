af771686bb93a364080260a1d17d466416006c30
timiajayi <timiajayi2001@gmail.com>
2025-02-12 16:49:33 +0100

    route

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,6 @@ Route::post('/auth/login', [AuthController::class, 'login']);
 Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])
     ->middleware(['signed', 'throttle:6,1'])
     ->name('verification.verify');
-
 Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('reset-password', [AuthController::class, 'resetPassword']);
 
